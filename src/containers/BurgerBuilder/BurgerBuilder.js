45db6aeaeb6f03638b14963b0ccc2024e096aa25
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-02-08 13:32:28 +0100

    Burger builder controler

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class BuilderBuilder extends Component{
+    render(){
+        return (
+            <>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </>
+        );
+    }
+}
+
+export default BuilderBuilder;
