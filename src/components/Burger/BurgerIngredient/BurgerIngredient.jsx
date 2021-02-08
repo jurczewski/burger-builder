917aa2752d05e59493290212098f240a5e0d7d38
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-02-08 15:18:37 +0100

    Burger builder ingredient working

--- src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
@@ -1,43 +1,43 @@
-import React from "react";
-import "./BurgerIngredient.css";
-import PropTypes from "prop-types";
+import React from 'react';
+import './BurgerIngredient.css';
+import PropTypes from 'prop-types';
 
 const burgerIngredient = (props) => {
-  let ingredient = null;
+	let ingredient = null;
 
-  switch (props.type) {
-    case "bread-bottom":
-      ingredient = <div className="BreadBottom"></div>;
-      break;
-    case "bread-top":
-      ingredient = (
-        <div className="BreadTop">
-          <div className="Seeds1"></div>
-          <div className="Seeds2"></div>
-        </div>
-      );
-      break;
-    case "meat":
-      ingredient = <div className="Meat"></div>;
-      break;
-    case "cheese":
-      ingredient = <div className="Cheese"></div>;
-      break;
-    case "salad":
-      ingredient = <div className="Salad"></div>;
-      break;
-    case "bacon":
-      ingredient = <div className="Bacon"></div>;
-      break;
-    default:
-      ingredient = null;
-  }
+	switch (props.type) {
+		case 'bread-bottom':
+			ingredient = <div className="BreadBottom" />;
+			break;
+		case 'bread-top':
+			ingredient = (
+				<div className="BreadTop">
+					<div className="Seeds1" />
+					<div className="Seeds2" />
+				</div>
+			);
+			break;
+		case 'meat':
+			ingredient = <div className="Meat" />;
+			break;
+		case 'cheese':
+			ingredient = <div className="Cheese" />;
+			break;
+		case 'salad':
+			ingredient = <div className="Salad" />;
+			break;
+		case 'bacon':
+			ingredient = <div className="Bacon" />;
+			break;
+		default:
+			ingredient = null;
+	}
 
-  return ingredient;
+	return ingredient;
 };
 
 burgerIngredient.propTypes = {
-  type: PropTypes.string.isRequired,
+	type: PropTypes.string.isRequired,
 };
 
 export default burgerIngredient;
