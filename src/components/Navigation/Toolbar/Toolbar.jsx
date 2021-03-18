b32abaecd84913463752900eaf8035fb4e60ab5b
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-03-18 23:11:17 +0100

    Navigation items

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = () => {
 	return (
 		<header className="Toolbar">
 			<div>MENU</div>
 			<Logo />
-			<nav>...</nav>
+			<nav>
+				<NavigationItems />
+			</nav>
 		</header>
 	);
 };
