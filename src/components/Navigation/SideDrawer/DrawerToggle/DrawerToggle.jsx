022e694bd5d3345624cf4d503eaafeff4c938410
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-04-22 16:10:40 +0200

    New menu icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
+import './DrawerToggle.css';
 
-const drawerToggle = (props) => <div onClick={props.clicked}>MENU</div>;
+const drawerToggle = (props) => (
+	<div className="DrawerToggle" onClick={props.clicked}>
+		<div />
+		<div />
+		<div />
+	</div>
+);
 
 export default drawerToggle;
